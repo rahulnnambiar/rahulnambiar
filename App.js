3f4d15fe8919862300ebb5c07b976e465cb5071d
Rahul Naranthatta <rnaranthatta00@mylangara.ca>
2022-10-05 13:18:49 -0700

    File structure

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import AppStack from './src/components/stack/AppStack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <AppStack/>
+      <StatusBar style="light-content" />
+    </NativeBaseProvider>
   );
 }
 
